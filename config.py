6b1bbe275d8a0c1766ca521a914f8ef714bb9df0
Elora VPN <eloravpn@gmail.com>
2024-05-02 18:24:01 +0330

    Version 0.2

--- config.py
+++ config.py
@@ -13,7 +13,7 @@ requests.packages.urllib3.util.connection.HAS_IPV6 = False
 CSV_FILE_PATH = config("CSV_FILE_PATH", default="./valid_example.csv")
 
 
-ELORA_BASE_API_URL = config("ELORA_TOKEN_URL", default="http://localhost:8000")
+ELORA_BASE_API_URL = config("ELORA_BASE_API_URL", default="http://localhost:8000")
 ELORA_TOKEN_PATH = config("ELORA_TOKEN_PATH", default="/api/admin/token")
 ELORA_USER_NAME = config("ELORA_USER_NAME", default="admin")
 ELORA_PASSWORD = config("ELORA_PASSWORD", default="admin")
